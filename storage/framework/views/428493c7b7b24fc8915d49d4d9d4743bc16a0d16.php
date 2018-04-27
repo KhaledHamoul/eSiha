edd2250f4bb0284a627000d5ab070e07a33ab78b
OussamaBATOUCHE <oussama1207@outlook.fr>
2018-04-27 20:50:41 +0100

    Merge branch 'master' of https://github.com/KhaledHamoul/eSiha

--- storage/framework/views/428493c7b7b24fc8915d49d4d9d4743bc16a0d16.php
+++ storage/framework/views/428493c7b7b24fc8915d49d4d9d4743bc16a0d16.php
@@ -12,15 +12,80 @@
 
       <!-- Modal content-->
       <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Modal Header</h4>
-        </div>
-        <div class="modal-body">
-          <p>Some text in the modal.</p>
-        </div>
+        
+        
+                        <div class="card card-outline-primary">
+                           
+                            <div class="card-body">
+                            
+                                <form action="#">
+                                    <div class="form-body">
+                                    <h3 class="box-title m-t-40">Nouvelle analyse / Nouveau radio</h3>
+                                        <hr>
+                                        
+                                        <div class="row">
+                                            <div class="col-md-12 ">
+                                                <div class="form-group">
+                                                    <label>Nom patient</label>
+                                                    <input type="text" class="form-control">
+                                                </div>
+                                            </div>
+                                        </div>
+                                         <div class="row">
+                                            <div class="col-md-12 ">
+                                                <div class="form-group">
+                                                    <label>Code patient</label>
+                                                    <input type="number" class="form-control">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Date</label>
+                                                    <input type="date" class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Time</label>
+                                                    <input type="time" class="form-control">
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                        <div class="row">
+                                                <div class="col-md-12">
+                                                            <label>Type</label>
+                                                            <select class="form-control">
+                                                                    <option>Analyse</option>
+                                                                    <option>Radio</option>
+                                                                </select>
+                                                </div>
+                                       </div>
+                                       <div class="row">
+                                          
+                                            <div class="col-md-12 ">
+                                                <div class="form-group">
+                                                    <br>
+                                                    <label>Description</label>
+                                                    <textarea style="height:250px" class="form-control"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-actions">
+                                        
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" onclick="removeDiv()" data-dismiss="modal">Close</button>
+           <a href="labo_analyse_radio"><button type="submit"  class="btn btn-success"> <i class="fa fa-check"></i> Enregistrer</button></a>
+           <button type="button" class="btn btn-inverse btn-default" onclick="removeDiv()" data-dismiss="modal">Annuler</button>
+          
         </div>
       </div>
 
@@ -38,7 +103,7 @@
           </div>
         <!-- End Page wrapper  -->
 
-
+<script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
 <script>
 
 removeDiv = function () {
@@ -46,5 +111,18 @@ removeDiv = function () {
     //alert('kjfd');
 }
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC78J7i4sucV9i5H0xMSusf8jYBDbF1JgI",
+    authDomain: "esiha-5e928.firebaseapp.com",
+    databaseURL: "https://esiha-5e928.firebaseio.com",
+    projectId: "esiha-5e928",
+    storageBucket: "esiha-5e928.appspot.com",
+    messagingSenderId: "1033888860300"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
 </script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
