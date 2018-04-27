4749c480fafa11b4bf77c3c1380284bd0fc11df5
KhaledHamoul <fk_hamoul@esi.dz>
2018-04-27 11:29:36 +0100

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,21 @@
 @include('layout.top')
 @include('layout.header')
 
-<<<<<<< HEAD
-@if($teypeUser == "medecin")
+
+@if($typeUser == "medecin")
   @include('layout.aside_menu_medecin')
   @include('layout.medecin_page_content')
-@elseif($teypeUser == "medecin-dash")
+@elseif($typeUser == "medecin-dash")
   @include('layout.aside_menu_medecin')
   @include('layout.medecin_page_content')
-@elseif($teypeUser == "medecin-profile")
+@elseif($typeUser == "medecin-profile")
   @include('layout.aside_menu_medecin')
   @include('layout.medecin_profile_page')
-  @elseif($teypeUser == "medecin-notification")
+  @elseif($typeUser == "medecin-notification")
   @include('layout.aside_menu_medecin')
   @include('layout.medecin_notification_page')
-@elseif($teypeUser == "labo")
-||||||| merged common ancestors
-@if($teypeUser == "medecin")
- @include('layout.aside_menu_medecin')
- 
-@elseif($teypeUser == "labo")
-=======
-@if($typeUser == "medecin")
- @include('layout.aside_menu_medecin')
-
 @elseif($typeUser == "labo")
->>>>>>> 2b488110159800e218ad1b7df98ceb75ffc8f561
-  @include('layout.aside_menu_labo')
-  @include('layout.labo_page_content')
-@else
- @include('layout.aside_menu_clinique')
+  
 @endif
 
 
--- storage/framework/views/1393dc3233abadd61fc67607f2f798627752fe69.php
+++ storage/framework/views/1393dc3233abadd61fc67607f2f798627752fe69.php
@@ -1,4 +1,5 @@
-   <footer class="footer"> © 2018 All rights reserved. Template designed by <a href="https://colorlib.com">Colorlib</a></footer>
+  <!-- footer -->
+            <footer class="footer"> © 2018 All rights reserved. </> by <a href="https://colorlib.com">TOKYO TEAM</a></footer>
             <!-- End footer -->
         </div>
         <!-- End Page wrapper  -->
@@ -15,16 +16,39 @@
     <script src="js/sidebarmenu.js"></script>
     <!--stickey kit -->
     <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+
+
+    <script src="js/lib/datamap/d3.min.js"></script>
+    <script src="js/lib/datamap/topojson.js"></script>
+    <script src="js/lib/datamap/datamaps.world.min.js"></script>
+    <script src="js/lib/datamap/datamap-init.js"></script>
+
+    <script src="js/lib/weather/jquery.simpleWeather.min.js"></script>
+    <script src="js/lib/weather/weather-init.js"></script>
+    <script src="js/lib/owl-carousel/owl.carousel.min.js"></script>
+    <script src="js/lib/owl-carousel/owl.carousel-init.js"></script>
+
+
+    <script src="js/lib/chartist/chartist.min.js"></script>
+    <script src="js/lib/chartist/chartist-plugin-tooltip.min.js"></script>
+    <script src="js/lib/chartist/chartist-init.js"></script>
     <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
 
 
-    <!-- Amchart -->
-     <script src="js/lib/morris-chart/raphael-min.js"></script>
-    <script src="js/lib/morris-chart/morris.js"></script>
-    <script src="js/lib/morris-chart/dashboard1-init.js"></script>
+    <script src="js/lib/datatables/datatables.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js"></script>
+    <script src="js/lib/datatables/cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
+    <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
+    
 
+    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
 
-	<script src="js/lib/calendar-2/moment.latest.min.js"></script>
+    <script src="js/lib/calendar-2/moment.latest.min.js"></script>
     <!-- scripit init-->
     <script src="js/lib/calendar-2/semantic.ui.min.js"></script>
     <!-- scripit init-->
@@ -33,13 +57,6 @@
     <script src="js/lib/calendar-2/pignose.calendar.min.js"></script>
     <!-- scripit init-->
     <script src="js/lib/calendar-2/pignose.init.js"></script>
-
-    <script src="js/lib/owl-carousel/owl.carousel.min.js"></script>
-    <script src="js/lib/owl-carousel/owl.carousel-init.js"></script>
-    <script src="js/scripts.js"></script>
-    <!-- scripit init-->
-
-    <script src="js/custom.min.js"></script>
 
 </body>
 
