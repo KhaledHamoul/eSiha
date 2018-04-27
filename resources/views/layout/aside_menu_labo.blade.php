a047d648cb992588931fa2d47b517276b0a9400d
mohamed-amine-guerras <fm_guerras@esi.dz>
2018-04-27 04:23:20 +0100

    added the doctor's aside bar

---
+++ resources/views/layout/aside_menu_labo.blade.php
@@ -0,0 +1,39 @@
+ <!-- Left Sidebar  -->
+        <div class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                       
+                        <li class="nav-label">Accueil</li>
+                        <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Tableau de bord</span></a></li>
+              
+                        <li class="nav-label">profile</li>
+                            <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-hospital-o"></i><span class="hide-menu">Mon Profile</span></a>
+                                <ul aria-expanded="false" class="collapse">
+                                    <li><a href="index.html">Profile</a></li>
+                                    <li><a href="index1.html">Paramaitres</a></li>
+                                    <li><a href="index1.html">Sortir</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-label">Options</li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-envelope-o"></i><span class="hide-menu">Notifications</span></a></li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-calendar"></i><span class="hide-menu">Rendez-vous</span></a></li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-heart-o"></i><span class="hide-menu">Analyses / Radios</span></a></li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-users"></i><span class="hide-menu">Nos patients</span></a></li>
+              
+                      
+                       
+                       
+                       
+                        
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+        </div>
+        <!-- End Left Sidebar  -->
