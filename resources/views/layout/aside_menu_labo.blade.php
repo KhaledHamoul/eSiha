7c583c9470a3ff440834adec6e1a805e8867c2e2
KhaledHamoul <fk_hamoul@esi.dz>
2018-04-27 01:53:42 +0100

    add of auth stuffs

--- resources/views/layout/aside_menu_labo.blade.php
+++ resources/views/layout/aside_menu_labo.blade.php
@@ -6,13 +6,24 @@
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
+                       
                         <li class="nav-label">Accueil</li>
-                        <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Tableau de bord</span></a>
-                            
-                        </li>
-                        <li class="nav-label">paramaitres</li>
-                        <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-envelope"></i><span class="hide-menu">Email</span></a>    
-                        </li>
+                        <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Tableau de bord</span></a></li>
+              
+                        <li class="nav-label">profile</li>
+                            <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Mon Profile</span></a>
+                                <ul aria-expanded="false" class="collapse">
+                                    <li><a href="index.html">Profile</a></li>
+                                    <li><a href="index1.html">Paramaitres</a></li>
+                                    <li><a href="index1.html">Sortir</a></li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-label">Options</li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Rendez-vous</span></a></li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Analyses / Radios</span></a></li>
+                            <li> <a class="" href="#" aria-expanded="false"><i class="fa fa-tachometer"></i><span class="hide-menu">Nos patients</span></a></li>
+              
                       
                        
                        
